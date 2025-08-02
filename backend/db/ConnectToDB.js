ff7b776a4f2e1a90962c4cd6e1ff32e35dffc9c3
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-08-02 21:25:26 +0600

    upadates on backend

--- backend/db/ConnectToDB.js
+++ backend/db/ConnectToDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log('MongoDB connected!');
+        console.log('MongoDB is connected!');
 
     } catch (error) {
         console.error('MongoDB connection error:', error);
